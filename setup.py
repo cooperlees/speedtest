534a692b4bd3007341c4f6462bf4a468936e5928
Cooper Ry Lees <me@cooperlees.com>
2023-01-02 21:01:54 -0800

    Go >= 3.11

--- setup.py
+++ setup.py
@@ -32,10 +32,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     entry_points={"console_scripts": ["speedtest-wrapper = speedtest_wrapper:main"]},
     install_requires=["prometheus_client"],
 )
