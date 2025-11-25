22e8b83724906b3a0e960c8852ebd24593611345
Cooper Ry Lees <me@cooperlees.com>
2025-11-24 19:50:06 -0600

    Move to >= 3.14 + move to 25.11.24 to cut a new release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_long_desc() -> str:
 
 setup(
     name="speedtest_wrapper",
-    version="24.7.29",
+    version="25.11.24",
     description="Wrap the speedtest cli and exports stats for prometheus",
     long_description=get_long_desc(),
     long_description_content_type="text/markdown",
@@ -32,9 +32,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.14",
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.14",
     entry_points={"console_scripts": ["speedtest-wrapper = speedtest_wrapper:main"]},
     install_requires=["prometheus_client"],
 )
